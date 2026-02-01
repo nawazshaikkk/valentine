0010b9be7cc63c7a2183ffe3c31f244c2e705255
nawazshaikkk <shaiknawazali1@gmail.com>
2026-02-01 10:27:34 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+const noBtn = document.getElementById("noBtn");
+const yesBtn = document.getElementById("yesBtn");
+const card = document.getElementById("card");
+
+noBtn.addEventListener("mouseover", () => {
+  const x = Math.random() * 200 - 100;
+  const y = Math.random() * 100 - 50;
+  noBtn.style.transform = `translate(${x}px, ${y}px)`;
+});
+
+yesBtn.addEventListener("click", () => {
+  card.innerHTML = `
+    <h1>YAY!!! 🎉💖</h1>
+    <p>You said YES 😭</p>
+    <img src="sticker.png" width="200">
+  `;
+});
